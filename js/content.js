cbdd3e7774d2171061fd46068ba753731a8c2330
shion.t.fujie <tonaluna32966@gmail.com>
2020-06-08 17:54:46 +0900

    detach tabs

--- js/content.js
+++ js/content.js
@@ -1,1 +1,8 @@
-console.log(location)
+const port = chrome.runtime.connect({name: PORT_NAME_DEFAULT})
+
+document.onkeydown = ({code, shiftKey, ctrlKey}) => {
+    if (code == "ArrowDown" && shiftKey && ctrlKey) {
+        if (port === null) return
+        port.postMessage(MESSAGE_DETACH)
+    }
+}
---
+++ js/util/constants.js
@@ -0,0 +1,2 @@
+const PORT_NAME_DEFAULT = "WiNager default"
+const MESSAGE_DETACH = "detach"
