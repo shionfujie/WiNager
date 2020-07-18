fc368a7fc6b7ae7c8804858c7568d2015d77fefc
shion.t.fujie <tonaluna32966@gmail.com>
2020-07-18 23:48:00 +0900

    separate manipulation logic

--- js/background.js
+++ js/background.js
@@ -5,27 +5,3 @@ chrome.runtime.onConnect.addListener(({ name, onMessage }) => {
       else if (message.type == MESSAGE_MOVE) moveTabs(message.offset)
     });
 });
-
-function moveTabs(offset) {
-  function _moveTabs(idsAndIndices) {
-    const [[id, index], ...rest] = idsAndIndices
-    chrome.tabs.move(id, {index}, () => {
-      _moveTabs(rest)
-    })
-  }
-  chrome.tabs.query({ currentWindow: true }, tabs => {
-    const countOfTabs = tabs.length
-    const countOfPinned = tabs.filter(tab => tab.pinned).length
-    const countOfUnpinned = countOfTabs - countOfPinned
-    const highlightedTabs = tabs.filter(tab => tab.highlighted)
-    const newIndex = (index, pinned) => {
-      if (pinned) return (index + offset) % countOfPinned
-      else {
-        const relativeIndex = (index - countOfPinned + offset) % countOfUnpinned
-        return relativeIndex < 0 ? - 1 : countOfPinned + relativeIndex
-      }
-    }
-    const idsAndIndices = highlightedTabs.map(({ id, index, pinned }) => [id, newIndex(index, pinned)])
-    _moveTabs(offset > 0 ? idsAndIndices.reverse() : idsAndIndices)
-  });
-}
---
+++ js/tabs/moveTabs.js
@@ -0,0 +1,26 @@
+function moveTabs(offset) {
+  chrome.tabs.query({ currentWindow: true }, tabs => {
+    const countOfTabs = tabs.length;
+    const countOfPinned = tabs.filter(tab => tab.pinned).length;
+    const countOfUnpinned = countOfTabs - countOfPinned;
+    const highlightedTabs = tabs.filter(tab => tab.highlighted);
+    const newIndex = (index, pinned) => {
+      if (pinned) return (index + offset) % countOfPinned;
+      else {
+        const relativeIndex =
+          (index - countOfPinned + offset) % countOfUnpinned;
+        return relativeIndex < 0 ? -1 : countOfPinned + relativeIndex;
+      }
+    };
+    const idsAndIndices = highlightedTabs.map(({ id, index, pinned }) => 
+        [id,newIndex(index, pinned)]);
+    moveTabsIncrementally(offset > 0 ? idsAndIndices.reverse() : idsAndIndices);
+  });
+}
+
+function moveTabsIncrementally(idsAndIndices) {
+  const [[id, index], ...rest] = idsAndIndices;
+  chrome.tabs.move(id, { index }, () => {
+    moveTabsIncrementally(rest);
+  });
+}
