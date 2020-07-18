0b33267b1abfb9afd19303c7fccf1e064a4beb96
shion.t.fujie <tonaluna32966@gmail.com>
2020-07-18 23:44:52 +0900

    moveTabs()

--- js/background.js
+++ js/background.js
@@ -1,6 +1,31 @@
 chrome.runtime.onConnect.addListener(({ name, onMessage }) => {
   if (name == PORT_NAME_DEFAULT)
     onMessage.addListener(message => {
-      if (message == MESSAGE_DETACH) detachTabs();
+      if (message.type == MESSAGE_DETACH) detachTabs();
+      else if (message.type == MESSAGE_MOVE) moveTabs(message.offset)
     });
 });
+
+function moveTabs(offset) {
+  function _moveTabs(idsAndIndices) {
+    const [[id, index], ...rest] = idsAndIndices
+    chrome.tabs.move(id, {index}, () => {
+      _moveTabs(rest)
+    })
+  }
+  chrome.tabs.query({ currentWindow: true }, tabs => {
+    const countOfTabs = tabs.length
+    const countOfPinned = tabs.filter(tab => tab.pinned).length
+    const countOfUnpinned = countOfTabs - countOfPinned
+    const highlightedTabs = tabs.filter(tab => tab.highlighted)
+    const newIndex = (index, pinned) => {
+      if (pinned) return (index + offset) % countOfPinned
+      else {
+        const relativeIndex = (index - countOfPinned + offset) % countOfUnpinned
+        return relativeIndex < 0 ? - 1 : countOfPinned + relativeIndex
+      }
+    }
+    const idsAndIndices = highlightedTabs.map(({ id, index, pinned }) => [id, newIndex(index, pinned)])
+    _moveTabs(offset > 0 ? idsAndIndices.reverse() : idsAndIndices)
+  });
+}
--- js/content.js
+++ js/content.js
@@ -1,8 +1,12 @@
 const port = chrome.runtime.connect({name: PORT_NAME_DEFAULT})
 
 document.onkeydown = ({code, shiftKey, ctrlKey}) => {
+    if (port === null) return
     if (code == "ArrowDown" && shiftKey && ctrlKey) {
-        if (port === null) return
-        port.postMessage(MESSAGE_DETACH)
+        console.log("detachtabs")
+        port.postMessage(detachTab())
+    } else if ((code == "ArrowRight" || code == "ArrowLeft") && shiftKey && ctrlKey) {
+        console.log("movetabs")
+        port.postMessage(moveTab(code == "ArrowRight" ? 1 : - 1))
     }
 }
