5ccbbfe6a2df699b3a7274cc8a21e96871e04be7
shion.t.fujie <tonaluna32966@gmail.com>
2020-07-21 21:43:55 +0900

    keymap: ⌃ to ⌘

--- js/content.js
+++ js/content.js
@@ -1,10 +1,10 @@
 const port = chrome.runtime.connect({name: PORT_NAME_DEFAULT})
 
-document.onkeydown = ({code, shiftKey, ctrlKey}) => {
+document.onkeydown = ({code, shiftKey, ctrlKey, metaKey}) => {
     if (port === null) return
     if (code == "ArrowDown" && shiftKey && ctrlKey) {
         port.postMessage(detachTab())
-    } else if ((code == "ArrowRight" || code == "ArrowLeft") && shiftKey && ctrlKey) {
+    } else if ((code == "ArrowRight" || code == "ArrowLeft") && shiftKey && metaKey) {
         port.postMessage(moveTab(code == "ArrowRight" ? 1 : - 1))
     }
 }
