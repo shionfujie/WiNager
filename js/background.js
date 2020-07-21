bf067213eb4c2d689aa07c56d8319f32a52ff3c1
shion.t.fujie <tonaluna32966@gmail.com>
2020-07-21 22:06:37 +0900

    duplicateCurrentTab()

--- js/background.js
+++ js/background.js
@@ -3,5 +3,13 @@ chrome.runtime.onConnect.addListener(({ name, onMessage }) => {
     onMessage.addListener(message => {
       if (message.type == MESSAGE_DETACH) detachTabs();
       else if (message.type == MESSAGE_MOVE) moveTabs(message.offset)
+      else if (message.type == MESSAGE_DUPLICATE) duplicateCurrentTab()
     });
 });
+
+
+function duplicateCurrentTab() {
+  chrome.tabs.query({active: true, currentWindow: true}, ([tab]) => {
+      chrome.tabs.duplicate(tab.id)
+  })
+}
