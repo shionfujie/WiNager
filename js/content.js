04bd0bf4bce7a804044fc4a32468265c570979e8
shion.t.fujie <tonaluna32966@gmail.com>
2020-07-21 21:41:33 +0900

    remove unused lines

--- js/content.js
+++ js/content.js
@@ -3,10 +3,8 @@ const port = chrome.runtime.connect({name: PORT_NAME_DEFAULT})
 document.onkeydown = ({code, shiftKey, ctrlKey}) => {
     if (port === null) return
     if (code == "ArrowDown" && shiftKey && ctrlKey) {
-        console.log("detachtabs")
         port.postMessage(detachTab())
     } else if ((code == "ArrowRight" || code == "ArrowLeft") && shiftKey && ctrlKey) {
-        console.log("movetabs")
         port.postMessage(moveTab(code == "ArrowRight" ? 1 : - 1))
     }
 }
