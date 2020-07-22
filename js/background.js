55e41ad0ffcfab951368ce611f8597e71dce4350
shion.t.fujie <tonaluna32966@gmail.com>
2020-07-22 15:42:25 +0900

    separate implementation

--- js/background.js
+++ js/background.js
@@ -14,11 +14,3 @@ function duplicateCurrentTab() {
       chrome.tabs.duplicate(tab.id)
   })
 }
-
-function navigateToUnpinnedTab(offset) {
-  chrome.tabs.query({currentWindow: true}, tabs => {
-    const index = tabs.filter(tab => tab.pinned).length + offset
-    if (- 1 < index < tabs.length)
-      chrome.tabs.update(tabs[index].id, {active: true})
-  })
-}
