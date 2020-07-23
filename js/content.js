086a5f11eac91adbfc78f59bff51aabf81cd5087
shion.t.fujie <tonaluna32966@gmail.com>
2020-07-23 17:47:56 +0900

    resolve keymap conflict

--- js/content.js
+++ js/content.js
@@ -1,13 +1,14 @@
 const port = chrome.runtime.connect({name: PORT_NAME_DEFAULT})
 
 document.onkeydown = ({code, shiftKey, ctrlKey, altKey, metaKey}) => {
+    console.log(code)
     if (port === null) return
     if (code == "ArrowDown" && shiftKey && ctrlKey) {
         port.postMessage(detachTab())
-    } else if ((code == "ArrowRight" || code == "ArrowLeft") && shiftKey && metaKey) {
+    } else if ((code == "ArrowRight" || code == "ArrowLeft") && ctrlKey && altKey && metaKey) {
         port.postMessage(moveTab(code == "ArrowRight" ? 1 : - 1))
     } else if (code == "KeyD" && altKey)
         port.postMessage(duplicate())
-    else if (code.startsWith("Digit") && shiftKey && metaKey)
+    else if (code.startsWith("Digit") && ctrlKey && altKey && metaKey)
         port.postMessage(navigateUnpinned(parseInt(code[5]) - 1))
 }
