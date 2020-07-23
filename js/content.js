b8ed24e281cca25e2205fba651890fde5f5d78b9
shion.t.fujie <tonaluna32966@gmail.com>
2020-07-23 17:50:27 +0900

    replace arrow keys with bracket keys

--- js/content.js
+++ js/content.js
@@ -5,8 +5,8 @@ document.onkeydown = ({code, shiftKey, ctrlKey, altKey, metaKey}) => {
     if (port === null) return
     if (code == "ArrowDown" && shiftKey && ctrlKey) {
         port.postMessage(detachTab())
-    } else if ((code == "ArrowRight" || code == "ArrowLeft") && ctrlKey && altKey && metaKey) {
-        port.postMessage(moveTab(code == "ArrowRight" ? 1 : - 1))
+    } else if ((code == "BracketRight" || code == "BracketLeft") && ctrlKey && altKey && metaKey) {
+        port.postMessage(moveTab(code == "BracketRight" ? 1 : - 1))
     } else if (code == "KeyD" && altKey)
         port.postMessage(duplicate())
     else if (code.startsWith("Digit") && ctrlKey && altKey && metaKey)
