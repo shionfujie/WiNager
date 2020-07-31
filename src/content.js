dca5ecb4bea4557b0a34870ed294782d0ac6b2df
shion.t.fujie <tonaluna32966@gmail.com>
2020-07-31 21:49:38 +0900

    fix ellipsis

--- src/content.js
+++ src/content.js
@@ -311,7 +311,7 @@ function StashEntry({title, url}) {
   return (
     <div class="shade-087 font-size-medium font-weight-medium padding-vertical-smaller line-height-medium">
       <div
-        class="pointer overflow-hidden white-space-nowrap text-ellipsis"
+        class="pointer overflow-ellipsis"
         title={`${title} - ${url}`}
       >
         {title}
