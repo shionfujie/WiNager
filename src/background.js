102f578a9a6067251cc0b2487ed9291d8e8fe6c5
shion.t.fujie <tonaluna32966@gmail.com>
2020-08-17 20:32:54 +0900

    reloadTabs

--- src/background.js
+++ src/background.js
@@ -130,3 +130,16 @@ function updateTabs(tabIds, updates, callback) {
   }
   if (tabIds.length > 0) _updateTabs(tabIds, updates, [], callback)
 }
+
+function reloadTabs(tabIds, callback) {
+  function _reloadTabs(tabIds, callback) {
+    if (tabIds.length === 0) callback && callback()
+    else {
+      const [tabId, ...restOfTabIds] = tabIds
+      chrome.tabs.reload(tabId, () => {
+        _reloadTabs(restOfTabIds, callback)
+      })
+    }
+  }
+  if (tabIds.length > 0) _reloadTabs(tabIds, callback)
+}
