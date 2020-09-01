3aeaddebac9404f2f96b5cebe1a0b122e409e5b1
shion.t.fujie <tonaluna32966@gmail.com>
2020-09-01 18:58:06 +0900

    symmetric spacing of entries

--- src/components/RestoreButton.js
+++ src/components/RestoreButton.js
@@ -2,7 +2,7 @@ import React from "react";
 
 export default function RestoreButton({ count, onClick }) {
   return (
-    <div class="inline-block padding-top-larger padding-bottom-small">
+    <div class="inline-block padding-top-medium padding-bottom-small">
       <div
         onClick={onClick}
         class="pointer underline-on-hover font-size-medium font-weight-medium line-height-medium winager-primary"
